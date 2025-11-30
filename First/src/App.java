8ebc8495b0cf2fb038f703a1bb4fd211b84beabb
bdvktr <bedyviktor2@gmail.com>
2025-11-30 14:12:58 +0100

    Delete First directory

--- First/src/App.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        user user1 = new user("lami", "lami@gmail.com", "Asder" );
- }
-}
